0af4c6de42fa351ddff9334d0a05c119d4067bf1
zeze1004 <thwjd2717@naver.com>
2020-11-14 14:13:09 +0900

    db 연동

--- nodejs_exercise/server.js
+++ nodejs_exercise/server.js
@@ -1,3 +1,4 @@
+const { json } = require('express')
 const express = require('express')
 const app = express()
 
@@ -15,5 +16,30 @@ app.get('/', function (req, res) { // app.get('라우터')
 app.get('/about', function (req, res) {
     res.render('about.html')
 })
-
+// 디비 연결
+var mysql = require('mysql');
+var pool  = mysql.createPool({
+  connectionLimit : 10,
+  host            : 'example.org',
+  user            : 'bob',
+  password        : 'secret',
+  database        : 'my_db'
+});
     
+app.get('/db', function (req, res) {
+    pool.getConnection(function(err, connection) {
+    if (err) {throw err;} // not connected!
+
+    // Use the connection
+    connection.query('SELECT * FROM test', function (error, results, fields) {
+        res.send(json.stringify(results))
+        // When done with the connection, release it.
+        connection.release();
+
+        // Handle error after the release.
+        if (error) throw error;
+
+        // Don't use the connection here, it has been returned to the pool.
+        });
+    });
+})
