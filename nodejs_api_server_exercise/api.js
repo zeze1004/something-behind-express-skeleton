931a49794fbc5f451a30ef22951be1f962191850
zeze1004 <thwjd2717@naver.com>
2020-11-14 15:05:01 +0900

    uuid-apikey 연습

--- nodejs_api_server_exercise/api.js
+++ nodejs_api_server_exercise/api.js
@@ -1,12 +1,49 @@
 const express = require('express')
 const app = express()
+const uuidAPIkey = require('uuid-apikey') // api 키를 db에서 관리
 const server = app.listen(3001, () =>{
     console.log('start server: localhost: 3001')
 })
 
-app.get('/api/users/:type', async (req, res) => { // :이 있는 paht는 어떤 값이든 받을 수 있다
-    let {type} = req.params
+console.log(uuidAPIkey.create())
+const key = { // db가 없으므로 변수로 관리
+    apiKey: 'W72TTDJ-3WVMJB3-KSV56ES-7DD5G6X', 
+    uuid: 'e1c5ad36-1f37-492c-9e76-533b3b5a581b'
+}
+
+app.get('/api/users/:apikey/:type', async (req, res) => { // :이 있는 paht는 어떤 값이든 받을 수 있다
+    let {
+        apiKey,
+        type
+    } = req.params
     console.log(type)
-    res.send('ok')
-    // /api/users/1 이라고 주소창에 쳤을 시 type = 1, 콘솔창에 1 출력, 클라이언트 창에 ok 출력
+    if(type == 'seoul') {
+        let data = [
+            // 예) 디비에 있는 서울새럼 정보
+            {
+                name:"홍길동" , 
+                city: "seoul",
+            },
+            {
+                name:"홍길순" ,
+                city: "seoul",
+            }
+        ]
+        res.send(data)
+    } else if(type == 'jeju') {
+        let data = [
+            // 디비에 있는 서울새럼 정보
+            {
+                name:"뾰로롱" , 
+                city: "jeju"
+            },
+            {
+                name:"큐큐큐" , 
+                city: "jeju"
+            }
+        ]
+        res.send(data)
+    } else {
+        res.send('type is not correct')
+    }
 })
